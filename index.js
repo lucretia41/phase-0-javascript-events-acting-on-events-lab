efac880ab0b90c3349a562ec9040bd3e67e2be4f
Ian Scott Martin <ianscottmartin@gmail.com>
2023-04-04 16:19:10 -0600

    finit

--- index.js
+++ index.js
@@ -6,36 +6,36 @@ function moveDodgerLeft() {
     if (left > 0) {
         dodger.style.left = `${left - 1}px`
     }
- }
+}
 
- function moveDodgerRight() {
+function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace('px', '');
     let left = parseInt(leftNumbers, 10)
     if (left > 0) {
         dodger.style.left = `${left + 1}px`
     }
- }
+}
 
 
 document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowLeft") {
         moveDodgerLeft()
     }
-  
-  })
+
+})
 
 
 
 
 
- document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowRight") {
         moveDodgerRight()
     }
-  
-  })
+
+})
 
 
 
- 
+
 
